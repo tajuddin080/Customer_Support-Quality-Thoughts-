ffdfc8460745a90926080435e4332392dae7a087
Tajuddin <tajuddinshaik080@gmail.com>
2026-04-21 10:25:04 +0530

    Added some changes

--- get_llm.py
+++ get_llm.py
@@ -1,4 +1,5 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chat_models.base import BaseChatModel
 import os
 from dotenv import load_dotenv
 
@@ -9,7 +10,7 @@ project = os.getenv("GOOGLE_CLOUD_PROJECT")
 
 model = "gemini-2.5-flash-lite"
 
-def get_llm(model:str):
+def get_llm(model:str)-> BaseChatModel:
     
     return (ChatGoogleGenerativeAI(
     model = model,
